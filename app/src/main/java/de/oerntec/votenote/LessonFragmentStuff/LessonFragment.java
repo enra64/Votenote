f10b61a474a5fa71dcc7f0bd92bb52149368d4f3
Arne <sujomear@arcor.de>
2015-11-19 20:16:25 +0100

    trying to implement tabs, is somewhat wonky

--- app/src/main/java/de/oerntec/votenote/LessonFragmentStuff/LessonFragment.java
+++ app/src/main/java/de/oerntec/votenote/LessonFragmentStuff/LessonFragment.java
@@ -19,11 +19,11 @@ package de.oerntec.votenote.LessonFragmentStuff;
 
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
